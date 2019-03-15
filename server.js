08a1d71260935a16cce7a483cdd64e7d37eb0c55
Jessica Madamba <jessica.madamba@gmail.com>
2019-03-15 10:54:26 -0700

    Add load balanced services

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(compression());
 // app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/photos', proxy({
-  target: 'http://ec2-52-15-166-74.us-east-2.compute.amazonaws.com'
+  target: 'http://ec2-18-219-98-0.us-east-2.compute.amazonaws.com'
 }));
 app.use('/api/reserve', proxy({
-  target: 'http://ec2-18-191-171-76.us-east-2.compute.amazonaws.com/'
+  target: 'http://ec2-18-217-214-176.us-east-2.compute.amazonaws.com'
 }));
 app.use('/menu', proxy({
   target: 'http://ec2-54-166-217-205.compute-1.amazonaws.com'
 }));
 app.use('/overview', proxy({
-  target: 'http://ec2-54-183-204-115.us-west-1.compute.amazonaws.com'
+  target: 'http://ec2-52-53-226-76.us-west-1.compute.amazonaws.com'
 }));
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
